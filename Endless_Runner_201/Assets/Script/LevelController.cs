6acd6290c99d0e982a691f1201afd0fe05c5d5f3
Anthony Feliz <47192556+arfgit@users.noreply.github.com>
2019-03-11 00:39:16 -0400

    endless run

---
+++ Endless_Runner_201/Assets/Script/LevelController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelController : MonoBehaviour{
+
+    public float scrollSpeed = 5.0f;
+    public GameObject[] floors;
+    public float frequency = 0.5f;
+    float counter = 0.0f;
+    public Transform challengesSpawnPoint;
+
+    void Start(){
+    GenerateFloorChallenge();
+
+    }
+
+    
+    void Update(){
+
+        //generate
+
+        if(counter <= 0.0f){
+        GenerateFloorChallenge();
+
+        } else
+        {
+            counter -= Time.deltaTime * frequency;
+        }
+    
+
+        //scroll
+         GameObject currentChild;
+        
+        for(int i = 0; i< transform.childCount; i++){
+
+            currentChild = transform.GetChild(i).gameObject;
+            ScrollChallenge(currentChild);
+            if(currentChild.transform.position.x <= -45.0f){
+
+            Destroy(currentChild);
+            }
+        }
+
+
+
+
+    }
+
+    void ScrollChallenge(GameObject currentChallenge){
+            currentChallenge.transform.position -= Vector3.right * (scrollSpeed * Time.deltaTime);
+
+            
+        
+    }
+    void GenerateFloorChallenge(){
+        GameObject newLevel = Instantiate (floors[Random.Range(0, floors.Length)], challengesSpawnPoint.position, Quaternion.identity) as GameObject;
+        newLevel.transform.parent = transform;
+        counter = 1.0f;
+
+    
+    }
+}
---
+++ Endless_Runner_201/Assets/Script/PlayerController.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour{  
+     public float speed; //controls the characters movement speed
+     public float jumpForce; //controls the characters jump
+     private float moveInput; //key movement 
+     private Rigidbody2D rb; //rigidbody
+
+     private bool facingRight = true; //for when we implement a sprite for the character
+
+     private bool isGrounded; 
+     public Transform groundCheck; //checks to see if character is grounded
+     public float checkRadius; 
+     public LayerMask whatIsGround;
+
+     private int numJump; //how many jumps does the player get
+    public int numJumpValue; //Set the value for the jumps
+
+      void Start(){
+        rb = GetComponent<Rigidbody2D>();
+
+
+    }
+
+    
+    void FixedUpdate(){
+
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
+
+
+
+
+        moveInput = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+
+  /*  if(facingRight == false && moveInput > 0){
+
+        Flip();
+    }
+    if(facingRight == true && moveInput > 0){ //this is for when we add a better                                                    character sprite
+         Flip();
+    } */
+
+
+    }
+
+    void Update(){
+
+        if(isGrounded == true){
+            numJump = numJumpValue;
+
+        }
+
+        if(Input.GetKeyDown(KeyCode.W) && numJump > 0){
+            rb.velocity = Vector2.up * jumpForce;
+            numJump--;
+
+
+        }
+
+        if(Input.GetKeyDown(KeyCode.W) && numJump == 0 && isGrounded == true){
+            rb.velocity = Vector2.up * jumpForce;
+
+        }
+
+    }
+
+    void Flip(){
+facingRight = !facingRight;
+Vector3 Scaler = transform.localScale;
+Scaler.x *= -1;
+transform.localScale = Scaler;
+
+
+    }
+
+  
+}
